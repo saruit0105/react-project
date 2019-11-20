70932b24da57ece1d2123171f1ff1ec77f2b66f5
saruit0105 <saruits2@gmail.com>
2019-11-20 15:04:36 -0500

    folders

---
+++ build/precache-manifest.0c832d7495b4bf76b93f972a983313e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "943e9ee3f47f5cd71cc566c27b70a6c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef96077a5474c855f370",
+    "url": "/static/css/main.1e7ca124.chunk.css"
+  },
+  {
+    "revision": "037c3492ef7fc939fcb3",
+    "url": "/static/js/2.03e86600.chunk.js"
+  },
+  {
+    "revision": "ef96077a5474c855f370",
+    "url": "/static/js/main.2fd73a22.chunk.js"
+  },
+  {
+    "revision": "b35138cac16ebf50b5c4",
+    "url": "/static/js/runtime-main.9e448be7.js"
+  },
+  {
+    "revision": "8069cc5197b680d5df8f7d554bae2260",
+    "url": "/static/media/istockphoto-935971392-612x612.8069cc51.jpg"
+  }
+]);
