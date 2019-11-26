0e1d673c8056795abd0d53ab7476fafde536736a
saruit0105 <saruits2@gmail.com>
2019-11-26 16:35:36 -0500

    Updates

---
+++ precache-manifest.c48958995c9673e2416c85319f65c577.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258b5d79e1ce1fbd1e22a79a8171d66a",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "0cb71a633a3434f2a501",
+    "url": "/react-project/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "4a3f205eb0a6a6deee48",
+    "url": "/react-project/static/css/main.57890ee6.chunk.css"
+  },
+  {
+    "revision": "0cb71a633a3434f2a501",
+    "url": "/react-project/static/js/2.00970721.chunk.js"
+  },
+  {
+    "revision": "4a3f205eb0a6a6deee48",
+    "url": "/react-project/static/js/main.976ca0eb.chunk.js"
+  },
+  {
+    "revision": "9000aba76f3ec246dd6a",
+    "url": "/react-project/static/js/runtime-main.1ed16372.js"
+  },
+  {
+    "revision": "bc9c83212a5cea71993c1316c8a086a3",
+    "url": "/react-project/static/media/goodplate.bc9c8321.png"
+  },
+  {
+    "revision": "8069cc5197b680d5df8f7d554bae2260",
+    "url": "/react-project/static/media/istockphoto-935971392-612x612.8069cc51.8069cc51.jpg"
+  }
+]);
